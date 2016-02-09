fbdc09e71064c7cad579380059d7437078e3cd5c
Kevia <cloud@kevia.me>
2016-02-09 14:58:10 -0800

    remove boiler plate in viewcontroller

---
+++ iFind/iFind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iFind
+//
+//  Created by jhampac on 2/9/16.
+//  Copyright © 2016 jhampac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+}
+
