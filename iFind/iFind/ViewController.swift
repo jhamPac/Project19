d9695c714964a444b2bd898216ca830a8008faab
Kevia <cloud@kevia.me>
2016-02-09 16:15:47 -0800

    tried using beacon location but it didn't work

--- iFind/iFind/ViewController.swift
+++ iFind/iFind/ViewController.swift
@@ -7,12 +7,82 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController
+class ViewController: UIViewController, CLLocationManagerDelegate
 {
+    var locationManager: CLLocationManager!
+    
+    @IBOutlet weak var distanceReading: UILabel!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        view.backgroundColor = UIColor.grayColor()
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus)
+    {
+        if status == .AuthorizedWhenInUse
+        {
+            if CLLocationManager.isMonitoringAvailableForClass(CLBeacon.self)
+            {
+                if CLLocationManager.isRangingAvailable()
+                {
+                    startScanning()
+                }
+            }
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion)
+    {
+        if beacons.count > 0
+        {
+            let beacon = beacons[0]
+            updateDistance(beacon.proximity)
+        }
+        else
+        {
+            updateDistance(.Unknown)
+        }
+    }
+    
+    func startScanning()
+    {
+        let uuid = NSUUID(UUIDString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, identifier: "homeBeacon")
+        locationManager.startMonitoringForRegion(beaconRegion)
+        locationManager.startRangingBeaconsInRegion(beaconRegion)
+    }
+    
+    func updateDistance(distance: CLProximity)
+    {
+        UIView.animateWithDuration(0.8) { [unowned self] in
+            switch distance {
+            case .Unknown:
+                self.view.backgroundColor = UIColor.grayColor()
+                self.distanceReading.text = "UNKNOWN"
+                
+            case .Far:
+                self.view.backgroundColor = UIColor.blueColor()
+                self.distanceReading.text = "FAR"
+                
+            case .Near:
+                self.view.backgroundColor = UIColor.orangeColor()
+                self.distanceReading.text = "NEAR"
+                
+            case .Immediate:
+                self.view.backgroundColor = UIColor.redColor()
+                self.distanceReading.text = "RIGHT HERE"
+            }
+            
+        }
     }
 
 }
